d63abe83e8e1f74ad5488b13dfd00ae8edd05467
xpres <xpresi@hotmail.com>
2026-04-30 10:40:14 +0200

    fix: base url corregida al nombre del repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/diagrama-conexiones/',
+  base: '/diagrama-instalaciones/',
   plugins: [
     tailwindcss(),
     react(),
